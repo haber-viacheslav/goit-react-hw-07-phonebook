dfb10570559348722b214407cf63f06fb3a5ae2d
Viacheslav Haber <gaberslavik@gmail.com>
2023-02-23 00:13:31 +0200

    added new features

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,26 +1,30 @@
-import ContactForm from './ContactForm';
-import ContactList from './ContactList';
-import Filter from './Filter';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchContacts } from 'redux/contactService';
+import { getContacts } from 'redux/selectors';
+// import ContactForm from './ContactForm';
+// import ContactList from './ContactList';
+// import Filter from './Filter';
 
 export const App = () => {
+  const dispatch = useDispatch();
+
+  const { items, isLoading, error } = useSelector(getContacts);
+
+  useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch]);
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex-start',
-        padding: '40px',
-        flexDirection: 'column',
-        gap: '40px',
-        color: '#ffffff',
-      }}
-    >
-      <div>
-        <h1>Phonebook</h1>
-        <ContactForm />
-        <h2>Contacts</h2>
-        <Filter />
-        <ContactList />
-      </div>
+    <div>
+      {/* <h1>Phonebook</h1>
+      <ContactForm />
+      <h2>Contacts</h2>
+      <Filter />
+      <ContactList /> */}
+      {isLoading && <p>Loading tasks...</p>}
+      {error && <p>{error}</p>}
+      <p>{items.length > 0 && JSON.stringify(items, null, 2)}</p>
     </div>
   );
 };
--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -1,14 +1,16 @@
 import { createSlice } from '@reduxjs/toolkit';
-import { nanoid } from 'nanoid';
 
 const contactsSlice = createSlice({
   name: 'contacts',
-  initialState: [],
+  initialState: {
+    items: [],
+    isLoading: false,
+    error: null,
+  },
   reducers: {
     addContact(state, action) {
       const contact = {
         ...action.payload,
-        id: nanoid(),
       };
       state.push(contact);
     },
@@ -16,8 +18,46 @@ const contactsSlice = createSlice({
       const index = state.findIndex(contact => contact.id !== action.payload);
       state.splice(index, 1);
     },
+    fetchingInProgress(state) {
+      state.isLoading = true;
+    },
+    fetchingSuccess(state, action) {
+      state.isLoading = false;
+      state.error = null;
+      state.items = action.payload;
+    },
+    fetchingError(state, action) {
+      state.isLoading = false;
+      state.error = action.payload;
+    },
   },
 });
 
-export const { addContact, deleteContact } = contactsSlice.actions;
+export const {
+  addContact,
+  deleteContact,
+  fetchingInProgress,
+  fetchingSuccess,
+  fetchingError,
+} = contactsSlice.actions;
 export const contactsReducer = contactsSlice.reducer;
+// const contactsSlice = createSlice({
+//   name: 'contacts',
+//   initialState: [],
+//   reducers: {
+//     addContact(state, action) {
+//       const contact = {
+//         ...action.payload,
+//         id: nanoid(),
+//       };
+//       state.push(contact);
+//     },
+//     deleteContact(state, action) {
+//       const index = state.findIndex(contact => contact.id !== action.payload);
+//       state.splice(index, 1);
+//     },
+//   },
+// });
+
+// export const { addContact, deleteContact } = contactsSlice.actions;
+// export const contactsReducer = contactsSlice.reducer;
